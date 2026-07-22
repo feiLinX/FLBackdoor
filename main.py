6b5ace0e5dc4809fa4d23410c4941bd3703b3a0e
feiLinX <hjz0203x@gmail.com>
2026-07-22 01:42:25 -0400

    Uncomment __main__ entry point in main.py

--- main.py
+++ main.py
@@ -116,142 +116,142 @@ def init_model(nclients, args):
     return nets
 
 
-# if __name__ == "__main__":
-#     args = args_parser()
-#     print(args)
-#     #=============== Logging setup ===============
-#     mkdirs(args.logdir)
-#     mkdirs(args.ckptdir)
-#     mkdirs(os.path.join(args.ckptdir, args.aggregation))
+if __name__ == "__main__":
+    args = args_parser()
+    print(args)
+    #=============== Logging setup ===============
+    mkdirs(args.logdir)
+    mkdirs(args.ckptdir)
+    mkdirs(os.path.join(args.ckptdir, args.aggregation))
 
-#     if args.log_file_name is None:
-#         argument_path = 'experiment_arguments-%s' % datetime.datetime.now().strftime("%Y-%m-%d-%H%M-%S")
-#     else:
-#         argument_path = 'experiment_arguments-%s' % args.log_file_name
+    if args.log_file_name is None:
+        argument_path = 'experiment_arguments-%s' % datetime.datetime.now().strftime("%Y-%m-%d-%H%M-%S")
+    else:
+        argument_path = 'experiment_arguments-%s' % args.log_file_name
 
-#     argument_path = argument_path + '.json'
+    argument_path = argument_path + '.json'
 
-#     with open(os.path.join(args.logdir, argument_path), 'w') as f:
-#         json.dump(str(args), f)
+    with open(os.path.join(args.logdir, argument_path), 'w') as f:
+        json.dump(str(args), f)
 
-#     if args.log_file_name is None:
-#         args.log_file_name = 'experiment_log-%s' % (datetime.datetime.now().strftime("%Y-%m-%d-%H%M-%S"))
+    if args.log_file_name is None:
+        args.log_file_name = 'experiment_log-%s' % (datetime.datetime.now().strftime("%Y-%m-%d-%H%M-%S"))
 
-#     log_path = args.log_file_name + '.log'
-#     print('log path: ', log_path)
+    log_path = args.log_file_name + '.log'
+    print('log path: ', log_path)
 
-#     for handler in logging.root.handlers[:]:
-#         handler.close()
-#         logging.root.removeHandler(handler)
+    for handler in logging.root.handlers[:]:
+        handler.close()
+        logging.root.removeHandler(handler)
 
-#     logging.basicConfig(
-#         filename=os.path.join(args.logdir, log_path),
-#         format='%(asctime)s %(levelname)-8s %(message)s',
-#         datefmt='%m-%d %H:%M', level=logging.INFO, filemode='w')
+    logging.basicConfig(
+        filename=os.path.join(args.logdir, log_path),
+        format='%(asctime)s %(levelname)-8s %(message)s',
+        datefmt='%m-%d %H:%M', level=logging.INFO, filemode='w')
 
-#     logger = logging.getLogger()
-#     #=============== Logging setup ===============
+    logger = logging.getLogger()
+    #=============== Logging setup ===============
 
-#     #=============== Dataset setup ===============
-#     logger.info("Partitioning data")
-#     seed_everything(args.init_seed)
+    #=============== Dataset setup ===============
+    logger.info("Partitioning data")
+    seed_everything(args.init_seed)
 
-#     client2dataidx = partition_data(dataset=args.dataset, datadir=args.data_dir, partition=args.partition,
-#                                      n_clients=args.nclients, alpha=args.bias)
+    client2dataidx = partition_data(dataset=args.dataset, datadir=args.data_dir, partition=args.partition,
+                                     n_clients=args.nclients, alpha=args.bias)
     
-#     if args.adv_type == 'CDLS':
-#         adv_clients = args.bd_adv_clients if args.bd_adv_clients is not None else list(range(args.nbyz))
-#         donor_domains = args.bd_donor_domains if args.bd_donor_domains is not None else \
-#             [d for d in ['mnist', 'mnist_m', 'svhn', 'syn', 'usps'] if d != args.bd_domain]
+    if args.adv_type == 'CDLS':
+        adv_clients = args.bd_adv_clients if args.bd_adv_clients is not None else list(range(args.nbyz))
+        donor_domains = args.bd_donor_domains if args.bd_donor_domains is not None else \
+            [d for d in ['mnist', 'mnist_m', 'svhn', 'syn', 'usps'] if d != args.bd_domain]
 
-#         logger.info("Building cross-domain label-swap backdoor (target_label=%s, partition=%s, adv_clients=%s)"
-#                     % (str(args.bd_target_label), str(args.bd_partition), str(adv_clients)))
+        logger.info("Building cross-domain label-swap backdoor (target_label=%s, partition=%s, adv_clients=%s)"
+                    % (str(args.bd_target_label), str(args.bd_partition), str(adv_clients)))
     
-#         # Build poisoned train loaders for selected clients
-#         client2loaders, test_dl, backdoor_test_dl, train_poison_dl = build_digits_backdoor(
-#             args, client2dataidx, adv_clients, args.bd_target_label, args.bd_partition,
-#             domain=args.bd_domain, donor_domains=donor_domains,
-#             donor_pool_size=args.bd_donor_pool_size, max_search=args.bd_max_search, seed=args.init_seed)
+        # Build poisoned train loaders for selected clients
+        client2loaders, test_dl, backdoor_test_dl, train_poison_dl = build_digits_backdoor(
+            args, client2dataidx, adv_clients, args.bd_target_label, args.bd_partition,
+            domain=args.bd_domain, donor_domains=donor_domains,
+            donor_pool_size=args.bd_donor_pool_size, max_search=args.bd_max_search, seed=args.init_seed)
         
-#         global_train_dl, _ = get_dataloader(args, dataset=args.dataset, data_dir=args.data_dir,
-#                                          train_bs=args.batch_size, test_bs=args.batch_size)
-#     elif args.adv_type == 'None':
-#         logger.info("No attack selected; training on clean data only")
+        global_train_dl, _ = get_dataloader(args, dataset=args.dataset, data_dir=args.data_dir,
+                                         train_bs=args.batch_size, test_bs=args.batch_size)
+    elif args.adv_type == 'None':
+        logger.info("No attack selected; training on clean data only")
 
-#         client2loaders = {}
-#         for client_id in range(args.nclients):
-#             train_dl_local, _ = get_dataloader(args, dataset=args.dataset, data_dir=args.data_dir,
-#                                             train_bs=args.batch_size, test_bs=args.batch_size, dataidxs=client2dataidx[client_id])
-#             client2loaders[client_id] = train_dl_local
+        client2loaders = {}
+        for client_id in range(args.nclients):
+            train_dl_local, _ = get_dataloader(args, dataset=args.dataset, data_dir=args.data_dir,
+                                            train_bs=args.batch_size, test_bs=args.batch_size, dataidxs=client2dataidx[client_id])
+            client2loaders[client_id] = train_dl_local
 
-#         global_train_dl, test_dl = get_dataloader(args, dataset=args.dataset, data_dir=args.data_dir,
-#                                                     train_bs = args.batch_size, test_bs = args.batch_size)
+        global_train_dl, test_dl = get_dataloader(args, dataset=args.dataset, data_dir=args.data_dir,
+                                                    train_bs = args.batch_size, test_bs = args.batch_size)
     
 
-#     # Random client sampling support
-#     clients_per_round = int(args.nclients * args.fraction)
-#     client_ls = [i for i in range(args.nclients)]
-#     client_ls_rounds = []
-#     if clients_per_round != args.nclients:
-#         for i in range(args.nrounds):
-#             client_ls_rounds.append(random.sample(client_ls, clients_per_round))
-#     else:
-#         for i in range(args.nrounds):
-#             client_ls_rounds.append(client_ls)
-#     #=============== Dataset setup =================
+    # Random client sampling support
+    clients_per_round = int(args.nclients * args.fraction)
+    client_ls = [i for i in range(args.nclients)]
+    client_ls_rounds = []
+    if clients_per_round != args.nclients:
+        for i in range(args.nrounds):
+            client_ls_rounds.append(random.sample(client_ls, clients_per_round))
+    else:
+        for i in range(args.nrounds):
+            client_ls_rounds.append(client_ls)
+    #=============== Dataset setup =================
 
-#     #=============== Model setup ===================
-#     logger.info("Initializing models")
-#     client2nets = init_model(args.nclients, args)
-#     global_net = init_model(1, args)[0]
-#     #=============== Model setup ===================
+    #=============== Model setup ===================
+    logger.info("Initializing models")
+    client2nets = init_model(args.nclients, args)
+    global_net = init_model(1, args)[0]
+    #=============== Model setup ===================
 
-#     #============== Training setup =================
-#     for comm_round in range(args.nrounds):
-#         logger.info("Communication round %d" % comm_round)
+    #============== Training setup =================
+    for comm_round in range(args.nrounds):
+        logger.info("Communication round %d" % comm_round)
 
-#         # local training on all selected clients
-#         nets_current = fedavg_local(args, global_net, logger, client2nets, client2loaders, client_ls_rounds, comm_round, test_dl)
+        # local training on all selected clients
+        nets_current = fedavg_local(args, global_net, logger, client2nets, client2loaders, client_ls_rounds, comm_round, test_dl)
 
-#         # global aggregation
-#         fedavg_global(global_net, client2loaders, nets_current)
+        # global aggregation
+        fedavg_global(global_net, client2loaders, nets_current)
 
-#         # compute ACC/ASR/train_asr
-#         global_net.cuda()
-#         train_acc, train_loss = compute_accuracy(global_net, global_train_dl)
-#         if args.adv_type == 'CDLS':
-#             test_acc, asr, train_asr = evaluate_acc_asr(global_net, test_dl, backdoor_test_dl, train_poison_dl)
-#             global_net.to('cpu')
+        # compute ACC/ASR/train_asr
+        global_net.cuda()
+        train_acc, train_loss = compute_accuracy(global_net, global_train_dl)
+        if args.adv_type == 'CDLS':
+            test_acc, asr, train_asr = evaluate_acc_asr(global_net, test_dl, backdoor_test_dl, train_poison_dl)
+            global_net.to('cpu')
 
-#             logger.info('>> Global Model Train Acc: %f' % train_acc)
-#             logger.info('>> Global Model Test ACC: %f' % test_acc)
-#             logger.info('>> Global Model Test ASR: %f' % asr)
-#             logger.info('>> Global Model Train-Poison ASR: %f' % train_asr)
-#             logger.info('>> Global Model Train Loss: %f' % train_loss)
+            logger.info('>> Global Model Train Acc: %f' % train_acc)
+            logger.info('>> Global Model Test ACC: %f' % test_acc)
+            logger.info('>> Global Model Test ASR: %f' % asr)
+            logger.info('>> Global Model Train-Poison ASR: %f' % train_asr)
+            logger.info('>> Global Model Train Loss: %f' % train_loss)
 
-#             if (comm_round + 1) % args.print_interval == 0:
-#                 print('round: ', str(comm_round))
-#                 print('>> Global Model Train accuracy: %f' % train_acc)
-#                 print('>> Global Model Test ACC: %f' % test_acc)
-#                 print('>> Global Model Test ASR: %f' % asr)
-#                 print('>> Global Model Train-Poison ASR: %f' % train_asr)
-#                 print('>> Global Model Train loss: %f' % train_loss)
-#         elif args.adv_type == 'None':
-#             test_acc, test_loss = compute_accuracy(global_net, test_dl)
-#             global_net.to('cpu')
+            if (comm_round + 1) % args.print_interval == 0:
+                print('round: ', str(comm_round))
+                print('>> Global Model Train accuracy: %f' % train_acc)
+                print('>> Global Model Test ACC: %f' % test_acc)
+                print('>> Global Model Test ASR: %f' % asr)
+                print('>> Global Model Train-Poison ASR: %f' % train_asr)
+                print('>> Global Model Train loss: %f' % train_loss)
+        elif args.adv_type == 'None':
+            test_acc, test_loss = compute_accuracy(global_net, test_dl)
+            global_net.to('cpu')
 
-#             logger.info('>> Global Model Train Acc: %f' % train_acc)
-#             logger.info('>> Global Model Test Acc: %f' % test_acc)
-#             logger.info('>> Global Model Train Loss: %f' % train_loss)
+            logger.info('>> Global Model Train Acc: %f' % train_acc)
+            logger.info('>> Global Model Test Acc: %f' % test_acc)
+            logger.info('>> Global Model Train Loss: %f' % train_loss)
 
-#             if (comm_round + 1) % args.print_interval == 0:
-#                 print('round: ', str(comm_round))
-#                 print('>> Global Model Train accuracy: %f' % train_acc)
-#                 print('>> Global Model Test accuracy: %f' % test_acc)
-#                 print('>> Global Model Train loss: %f' % train_loss)
+            if (comm_round + 1) % args.print_interval == 0:
+                print('round: ', str(comm_round))
+                print('>> Global Model Train accuracy: %f' % train_acc)
+                print('>> Global Model Test accuracy: %f' % test_acc)
+                print('>> Global Model Train loss: %f' % train_loss)
 
-#         if (comm_round + 1) % args.save_interval == 0:
-#             torch.save(global_net.state_dict(),
-#                 os.path.join(args.ckptdir, args.aggregation, 'globalmodel_'+args.log_file_name+'.pth'))
-#             torch.save(client2nets[0].state_dict(),
-#                 os.path.join(args.ckptdir, args.aggregation, 'localmodel0_'+args.log_file_name+'.pth'))
+        if (comm_round + 1) % args.save_interval == 0:
+            torch.save(global_net.state_dict(),
+                os.path.join(args.ckptdir, args.aggregation, 'globalmodel_'+args.log_file_name+'.pth'))
+            torch.save(client2nets[0].state_dict(),
+                os.path.join(args.ckptdir, args.aggregation, 'localmodel0_'+args.log_file_name+'.pth'))
